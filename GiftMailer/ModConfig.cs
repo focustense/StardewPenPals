be52f33584b20146ebfbb0c27be9b42bd0a9a7d3
focustense <focustense@protonmail.com>
2024-09-01 13:52:25 -0400

    Add project files.

---
+++ GiftMailer/ModConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace GiftMailer;
+
+/// <summary>
+/// Configuration settings for $safeprojectname$.
+/// </summary>
+public class ModConfig
+{
+}
---
+++ GiftMailer/ModEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace GiftMailer;
+
+internal sealed class ModEntry : Mod
+{
+    // Initialized in Entry
+    private ModConfig config = null!;
+
+    public override void Entry(IModHelper helper)
+    {
+        I18n.Init(helper.Translation);
+        config = helper.ReadConfig<ModConfig>();
+    }
+}
